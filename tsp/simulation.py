93f6c0f0b5740078a63d50851861649e6dcada83
Arjun Neervannan <arjun.neervannan@gmail.com>
2024-02-07 16:30:51 -0500

    generate points

--- tsp/simulation.py
+++ tsp/simulation.py
@@ -1,6 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def generate_points(num_points):
     np.random.seed(0)
-    np.random.shuffle(num_points)
-    
+    return np.random.rand(num_points, 2)
+
+
+test_points = generate_points(10)
+plt.scatter(test_points[:, 0], test_points[:, 1])
+plt.show()
