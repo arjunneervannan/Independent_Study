113db4014b7e058ec884535dd076d54c1ef60c3d
Arjun Neervannan <arjun.neervannan@gmail.com>
2024-02-14 18:01:01 -0500

    cleanup

--- tsp/brute_force_check.py
+++ tsp/brute_force_check.py
@@ -3,11 +3,20 @@ from algorithms import *
 num_iters = 10
 hilbert_order = 2
 
+distance_differences = []
+
 for _ in range(num_iters):
     points = generate_points(10)
-    # square_ranges = generate_hilbert_curve_squares(4)
-    final_order, min_distance = optimal_order(points)
+    tsp_optimal_order, _ = optimal_order(points)
     square_ranges = generate_hilbert_curve_squares(hilbert_order)
-    tsp_order = define_order(points, square_ranges)
-    display_path(tsp_order)
-    display_path(final_order)
+    hilbert_order = define_order(points, square_ranges)
+    tsp_optimal_distance = total_distance_squared(tsp_optimal_order)
+    hilbert_distance = total_distance_squared(hilbert_order)
+    print(f"Optimal TSP distance: {tsp_optimal_distance}") 
+    print(f"Hilbert TSP distance: {hilbert_distance}\n-----\n")
+    
+    distance_differences.append(math.abs(tsp_optimal_distance - hilbert_distance))
+
+print(f"average distance difference: {np.mean(distance_differences)}")
+    
+    
