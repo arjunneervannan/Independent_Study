9615d80257b141ba526d59fe3a4e67188f498804
Arjun Neervannan <arjun.neervannan@gmail.com>
2024-02-15 01:04:29 -0500

    fixed final bugs

--- martingales/branching/simulations.py
+++ martingales/branching/simulations.py
@@ -5,12 +5,13 @@ from tqdm import tqdm
 if __name__ == '__main__':
     num_iterations = 50
     N = [10, 50, 100, 500, 1000]
-    lambda_val = [0.3, 0.5, 1, 2]
+    lambda_vals = [0.3, 0.5, 1, 2]
     p_val = 0.3
-    mean_iterations = []
+    
     all_mean_iterations = []
     
-    for lambda_val in tqdm(lambda_val):
+    for lambda_val in lambda_vals:
+        mean_iterations = []
         for num_points in tqdm(N):
             avg_generations = []
             for iter in tqdm(range(num_iterations)):
@@ -21,10 +22,13 @@ if __name__ == '__main__':
             mean_iterations.append(np.mean(avg_generations))
         all_mean_iterations.append(mean_iterations)
     
-    for i in range(len(lambda_val)):
-        legend_label = f"Lambda: {lambda_val[i]}"
+    for i in range(len(lambda_vals)):
+        legend_label = f"Lambda: {lambda_vals[i]}"
         plt.plot(N, all_mean_iterations[i], label=legend_label)
         
+    plt.title("Average Number of Iterations over Increasing Number of People")
+    plt.xlabel("Number of People")
+    plt.ylabel("Average Number of Iterations")
     plt.legend()
     plt.show()
     
